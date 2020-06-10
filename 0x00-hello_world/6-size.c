97f68ff8e680075817526b053943a9adb56e68d2
serosc95 <oscardleon95@gmail.com>
2020-06-10 21:55:37 +0000

    commit sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,5 @@ int main(void)
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(double));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
-	return(0);
+	return (0);
 }
