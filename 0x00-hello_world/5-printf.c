e46fa8a51d211787f6cb117d253eda5e480c982a
serosc95 <oscardleon95@gmail.com>
2020-06-10 20:18:47 +0000

    commit printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+* main - using printf
+Â
+* Use the function printf
+*
+* You are not allowed to use the function puts
+*
+* Your program should return 0
+*
+* Your program should compile without warning when using the -Wall gcc option
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
