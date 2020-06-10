b1ccbaa5cdf911b8d69df3d3761308ffb6c05396
serosc95 <oscardleon95@gmail.com>
2020-06-10 19:31:07 +0000

    commit puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main - using puts
+ * @void: void
+ *
+ * Description: Uses puts to outputUse the function puts
+ *		You are not allowed to use printf
+ *		Your program should end with the value 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
