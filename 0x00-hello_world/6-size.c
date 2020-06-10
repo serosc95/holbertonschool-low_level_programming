dfcce209207d7e1e96e190282775394d9ef42121
serosc95 <oscardleon95@gmail.com>
2020-06-10 20:46:02 +0000

    commit sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - using sizeof
+ *
+ * You should produce the exact same output as in the example
+ *
+ * Warnings are allowed
+ *
+ * Your program should return 0
+ *
+ * You might have to install the package libc6-dev-i386 on your Linux (Vagrant) to test the -m32 gcc option
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(double));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return(0);
+}
