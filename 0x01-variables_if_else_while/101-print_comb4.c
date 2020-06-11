46f8905b0afdacbec3682d8cdf2c6d5453563abd
serosc95 <oscardleon95@gmail.com>
2020-06-11 22:20:39 +0000

    commit combination 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/**
+ * main - entry point for function
+ *
+ * Description: decides if a random number is odd or even
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i, n, m;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (n = 48; n < 58; n++)
+		{
+			for (m = 48; m < 58; m++)
+			{
+				if (m != n && m != i && n != i && m > n && m > i && n > i)
+				{
+					putchar(i);
+					putchar(n);
+					putchar(m);
+					if (m != 56 || n != 57 || i != 56)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
