bff44cf1a4e9b1c539f3e640e0da578a95e93e5c
serosc95 <oscardleon95@gmail.com>
2020-06-11 19:11:35 +0000

    commit variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
+/**
+ * main - entry point for function
+ *
+ * Description: decides if a random number is odd or even
+ *
+ * Return: always 0 (success)
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -10,10 +17,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%i is positive\n", n);
+		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("%i is zero\n", n);
+		printf("%d is zero\n", n);
 	else
-		printf("%i is negative\n", n);
+		printf("%d is negative\n", n);
 	return (0);
 }
