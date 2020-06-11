e251a85a8165bfed03612119931403c7faa5ea8c
serosc95 <oscardleon95@gmail.com>
2020-06-11 20:05:22 +0000

    commit Upside down

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - entry point for function
+ *
+ * Description: decides if a random number is odd or even
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
