0b7218c5b84b87baebc34ccc8c4d316e71a05b3c
serosc95 <oscardleon95@gmail.com>
2020-06-11 20:47:38 +0000

    commit combination numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - entry point for function
+ *
+ * Description: decides if a random number is odd or even
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(10);
+	return (0);
+}
