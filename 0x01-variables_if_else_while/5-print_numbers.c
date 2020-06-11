81a489c110c59f681e3a69807e64e79dbfb0fcf9
serosc95 <oscardleon95@gmail.com>
2020-06-11 19:59:03 +0000

    commit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - entry point for function
+ *
+ * Description: decides if a random number is odd or even
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
