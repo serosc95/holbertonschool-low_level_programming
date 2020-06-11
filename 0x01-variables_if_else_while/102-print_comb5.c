a96abfd8c0fda224344f069083bb3c60f50733d3
serosc95 <oscardleon95@gmail.com>
2020-06-11 23:58:23 +0000

    commit combination 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,30 +12,30 @@ int main(void)
 
 	for (i = 48; i < 58; i++)
 	{
-		for (n = 48; n < 58; n++)
-		{
-			for (m = 48; m < 58; m++)
-			{
-				for (p = 48; p < 58; p++)
-				{
-					aux1 = i * 10 + n;
-					aux2 = m * 10 + p;
-					if (aux1 < aux2)
-					{
-						putchar(i);
-						putchar(n);
-						putchar(32);
-						putchar(m);
-						putchar(p);
-						if (n != 56 || i != 57 || m != 57 || p != 57)
-						{
-							putchar(44);
-							putchar(32);
-						}
-					}
-				}
-			}
-		}
+	for (n = 48; n < 58; n++)
+	{
+	for (m = 48; m < 58; m++)
+	{
+	for (p = 48; p < 58; p++)
+	{
+	aux1 = i * 10 + n;
+	aux2 = m * 10 + p;
+	if (aux1 < aux2)
+	{
+	putchar(i);
+	putchar(n);
+	putchar(32);
+	putchar(m);
+	putchar(p);
+	if (n != 56 || i != 57 || m != 57 || p != 57)
+	{
+	putchar(44);
+	putchar(32);
+	}
+	}
+	}
+	}
+	}
 	}
 	putchar(10);
 	return (0);
