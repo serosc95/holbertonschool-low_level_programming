5d1a4e071aaaea3e505b48881b850085a3b15fe4
serosc95 <oscardleon95@gmail.com>
2020-06-11 19:06:28 +0000

    commit variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
@@ -9,16 +10,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
+		printf("%i is positive\n", n);
+	else if (n == 0)
+		printf("%i is zero\n", n);
+	else
+		printf("%i is negative\n", n);
 	return (0);
 }
