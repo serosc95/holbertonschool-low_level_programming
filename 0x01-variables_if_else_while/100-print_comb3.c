f11e7fc5761a97eb6d794e5ab7bec20cf7ce1f6f
serosc95 <oscardleon95@gmail.com>
2020-06-11 22:05:22 +0000

    commit combinacion 3 numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+ * main - entry point for function
+ *
+ * Description: decides if a random number is odd or even
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i, n;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (n = 48; n < 58; n++)
+		{
+			if (n != i && i < n)
+			{
+				putchar(i);
+				putchar(n);
+				if (n != 57 || i != 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
