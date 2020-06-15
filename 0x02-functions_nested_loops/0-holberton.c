53ec216557f3c0716936f876065f44e7920d58c3
serosc95 <oscardleon95@gmail.com>
2020-06-15 16:44:00 +0000

    commit putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include"holberton.h"
+/**
+ * main - print Holberton
+ *
+ * Return: On success 1.
+ */
+int main(void)
+{
+	_putchar(72);
+	_putchar(111);
+	_putchar(108);
+	_putchar(98);
+	_putchar(101);
+	_putchar(114);
+	_putchar(116);
+	_putchar(111);
+	_putchar(110);
+	_putchar(10);
+	return (0);
+}
