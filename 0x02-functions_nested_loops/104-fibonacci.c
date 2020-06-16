df62b202a7987cbc745df63a2b45d536865235bc
serosc95 <oscardleon95@gmail.com>
2020-06-16 16:30:30 +0000

    commit fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include"holberton.h"
+#include<stdio.h>
+/**
+ * main - prints the first 50 Fibonacci numbers
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	double suma = 1, a = 1, b;
+	int i;
+
+	for (i = 2; i < 100; i++)
+	{
+		b = suma;
+		suma = suma + a;
+		a = b;
+		printf("%f", a);
+		if (i != 51)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
