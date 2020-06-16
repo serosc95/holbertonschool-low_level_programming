045c67344e9ed0df450abfae2d3edb30133ea39a
serosc95 <oscardleon95@gmail.com>
2020-06-16 19:09:28 +0000

    commit debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,16 @@
 #include "holberton.h"
- /**
- * main - print positive or negative
- *
- * Return: 0
- */
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-	int n = 0;
+        int i;
 
-	positive_or_negative(n);
-	return (0);
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }
