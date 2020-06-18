c74661e8d32567d77fca39529c8102df07938a6c
serosc95 <oscardleon95@gmail.com>
2020-06-18 17:22:16 +0000

    commit corrections

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,13 +6,16 @@
  */
 void more_numbers(void)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i < 15; i++)
+	for (j = 0; j < 10; j++)
 	{
-		if (i > 9)
-			_putchar((i / 10) + '0');
-		_putchar((i % 10) + '0');
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 9)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar(10);
 	}
-	_putchar(10);
 }
