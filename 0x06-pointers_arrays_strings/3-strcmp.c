0ab5f56d3c4e9e1d33e7f81d4bffa232504fad26
serosc95 <oscardleon95@gmail.com>
2020-06-25 22:13:05 +0000

    commit three

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include"holberton.h"
+/**
+ * _strcmp - compares two strings.
+ * @s1: pointer.
+ * @s2: pointer.
+ * Return: result integer.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s2[i] != '\0'; i++)
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	return (0);
+}
