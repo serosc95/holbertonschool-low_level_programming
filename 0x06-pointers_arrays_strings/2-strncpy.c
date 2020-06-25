36cb572dc2cd43f6e9158876b8bbe3cd028360b2
serosc95 <oscardleon95@gmail.com>
2020-06-25 19:38:53 +0000

    commir second

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,9 +10,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
-	for (j = 0; j < n && src[j] != '\0'; j++)
-		dest[j] = src[j];
-	if (src[j] == '\0')
-		dest[j] = '\0';
+	for (j = 0; j < n; j++)
+	{
+		if (src[j] == '\0')
+		{
+                	dest[j] = '\0';
+			j = n;
+		}
+		else
+			dest[j] = src[j];
+	}
 	return (dest);
 }
