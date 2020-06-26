5f731b57404b48c94a2d1ce7e8cc81493b9c1b1e
serosc95 <oscardleon95@gmail.com>
2020-06-26 00:24:14 +0000

    commit seven

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,20 +6,14 @@
  */
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char letras[] = "aeotlAEOTL";
+	char numbers[] = {'4', '3', '0', '7', '1',
+				'4', '3', '0', '7', '1'};
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
-	}
+		for (j = 0; letras[j] != '\0'; j++)
+			if (s[i] == letras[j])
+				s[i] = numbers[j];
 	return (s);
 }
