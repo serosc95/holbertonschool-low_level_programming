cbd89bec7552660f30600a7b36482bda0a515ea1
serosc95 <oscardleon95@gmail.com>
2020-06-29 15:56:36 +0000

    commit one

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include"holberton.h"
+/**
+ * _memcpy - copies memory area.
+ * @dest: pointer.
+ * @src: pointer.
+ * @n: number
+ * Return: result pointer.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*dest++ = src[i];
+	return (dest);
+}
