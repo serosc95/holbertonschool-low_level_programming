cf07d8d155d62e52382b2e4627e2e40705b04388
serosc95 <oscardleon95@gmail.com>
2020-06-29 18:46:33 +0000

    commit third

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,18 +8,14 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
-	unsigned int contador = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-			{
-				contador++;
 				break;
-			}
 			else if (accept[j + 1] == '\0')
-				return (contador);
+				return (i);
 		}
 	return (0);
 }
