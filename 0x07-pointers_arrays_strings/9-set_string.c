6f665244f8163be969d113df0e303c4c7695a1b7
serosc95 <oscardleon95@gmail.com>
2020-06-30 00:07:54 +0000

    commit nine

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include"holberton.h"
+/**
+ * set_string - sets the value of a pointer to a char.
+ * @s: pointer.
+ * @to: pointer.
+ * Return: vacio.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
