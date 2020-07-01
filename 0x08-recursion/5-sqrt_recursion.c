54298c10cf10c5748fcc602add9d6bf152d4f510
serosc95 <oscardleon95@gmail.com>
2020-07-01 22:12:20 +0000

    commit five

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include"holberton.h"
+/**
+ * sqrt - natural square root of a number.
+ * @n: integer.
+ * @x: integer.
+ * Return: number integer.
+ */
+int _sqrt(int n, int x)
+{
+	int aux;
+
+	if (x * x < n)
+	{
+		aux = _sqrt(n, x + 1);
+		return(aux);
+	}
+	else if (x * x == n)
+		return (x);
+	return (-1);
+}
+/**
+ * _sqrt_recursion - natural square root of a number.
+ * @n: integer.
+ * Return: number integer.
+ */
+int _sqrt_recursion(int n)
+{
+	int aux;
+
+	if (n > 0)
+	{
+		aux = _sqrt(n, 1);
+		return (aux);
+	}
+	else if (n < 0)
+		return (-1);
+	return (1);
+}
