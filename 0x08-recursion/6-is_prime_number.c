4fd7e20a6e90d5b2725640ffaba637eec1646f4b
serosc95 <oscardleon95@gmail.com>
2020-07-01 23:18:21 +0000

    commit six

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,43 @@
+#include"holberton.h"
+/**
+ * number_prime - detect prime numbers.
+ * @n: integer.
+ * @x: integer.
+ * Return: number integer.
+ */
+int number_prime(int n, int a)
+{
+	int aux;
+
+	if (a < n)
+	{
+		if (n % a != 0)
+		{
+			aux = number_prime(n, a + 1);
+			return (aux);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	else
+		return (1);
+}
+/**
+ * is_prime_number - detect prime numbers.
+ * @n: integer.
+ * Return: number integer.
+ */
+int is_prime_number(int n)
+{
+	int aux;
+
+	if (n > 1)
+	{
+		aux = number_prime(n, 2);
+		return (aux);
+	}
+	else
+		return (0);
+}
