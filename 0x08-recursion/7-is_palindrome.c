de749aff130f6438d329f2d37bc9ad7332dabeaa
serosc95 <oscardleon95@gmail.com>
2020-07-02 03:16:45 +0000

    commit seven

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,50 @@
+#include"holberton.h"
+/**
+ * is_string - detect prime numbers.
+ * @s: pointer.
+ * Return: number integer.
+ */
+int is_string(char *s)
+{
+	int aux;
+
+	if (*s != '\0')
+	{
+		aux = is_string(++s);
+		return (aux + 1);
+	}
+	return (0);
+}
+/**
+ * is_palindrome - detect prime numbers.
+ * @s: pointer.
+ * Return: number integer.
+ */
+int _compare(char *s, int l1, int l2)
+{
+	if (l1 > l2)
+	{
+		if (s[l1] == s[l2])
+		{
+			l1--;
+			l2++;
+			_compare(s, l1, l2);
+		}
+		else
+			return (0);
+	}
+	return (1);
+}
+/**
+ * is_palindrome - detect prime numbers.
+ * @s: pointer.
+ * Return: number integer.
+ */
+int is_palindrome(char *s)
+{
+	int l1, x;
+
+	l1 = is_string(s);
+	x = _compare(s, l1 - 1, 0);
+	return (x);
+}
