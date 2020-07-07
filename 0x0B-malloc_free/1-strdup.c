f8231f9d6d605979cd1b13070729ff9d1aeadd85
serosc95 <oscardleon95@gmail.com>
2020-07-07 17:26:34 +0000

    commit one

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include"holberton.h"
+#include <stdlib.h>
+/**
+ * _strdup - allocated space in memory.
+ * @str: pointer.
+ * Return: string pointer.
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int count = 0, i;
+
+	if (str == '\0')
+		return ('\0');
+	while (str[count] != '\0')
+		count++;
+	s = malloc(count * sizeof(char));
+	if (s != '\0')
+	{
+		for (i = 0; i < count; i++)
+			s[i] = str[i];
+		return (s);
+	}
+	return ('\0');
+}
