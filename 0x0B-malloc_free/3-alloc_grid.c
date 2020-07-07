cc858cd4d51de0cff68566a1262d85e170246252
serosc95 <oscardleon95@gmail.com>
2020-07-07 22:09:35 +0000

    commit again

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,24 +12,24 @@ int **alloc_grid(int width, int height)
 	int **s, i, j;
 
 	if (width == '\0' || height == '\0')
-		return('\0');
+		return ('\0');
 	s = malloc(height * sizeof(int *));
 	if (s != '\0')
 	{
 		for (i = 0; i < height; i++)
 		{
 			s[i] = malloc(width * sizeof(int));
-			if (s[i] != '\0')
+			if (s[i] == '\0')
 			{
-				for (j = 0; j < width; j++)
-					s[i][j] = 0;
-			}
-			else
-			{
+				for (; i >= 0; i--)
+					free(s[i]);
 				free(s);
 				return ('\0');
 			}
 		}
+		for (i = 0; i < height; i++)
+			for (j = 0; j < width; j++)
+				s[i][j] = 0;
 		return (s);
 	}
 	return ('\0');
