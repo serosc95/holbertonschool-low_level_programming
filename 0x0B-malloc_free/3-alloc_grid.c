07625244bbbcc36d20c1710ad9a331c0690eb25a
serosc95 <oscardleon95@gmail.com>
2020-07-07 21:38:18 +0000

    commit three

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include"holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * alloc_grid -  pointer to a 2 dimensional of integers.
+ * @width: integer.
+ * @height: integer.
+ * Return: string pointer.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **s, i, j;
+
+	if (width == '\0' || height == '\0')
+		return('\0');
+	s = malloc(height * sizeof(int *));
+	if (s != '\0')
+	{
+		for (i = 0; i < height; i++)
+		{
+			s[i] = malloc(width * sizeof(int));
+			if (s[i] != '\0')
+			{
+				for (j = 0; j < width; j++)
+					s[i][j] = 0;
+			}
+			else
+				return ('\0');
+		}
+		return (s);
+	}
+	return ('\0');
+}
