12b7d9ca5e0c90cae2bcb39b3097a32a3c14cbe6
serosc95 <oscardleon95@gmail.com>
2020-07-07 23:48:50 +0000

    commit four

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include"holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * free_grid - pfrees a 2 dimensional grid.
+ * @grid: double pointer.
+ * @height: integer.
+ * Return: void.
+ */
+
+void free_grid(int **grid, int height)
+{
+	int **s, i;
+
+	if (grid != '\0' && height != 0)
+	{
+		for (i = 0; i < height; i++)
+			free(grid[i]);
+		free(grid);
+	}
+}
