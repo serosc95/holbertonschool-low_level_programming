ff3d033eb1d4c2e25494526a2ee0d86df69a9d11
serosc95 <oscardleon95@gmail.com>
2020-07-09 16:03:17 +0000

    commit one

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n > j)
 		n = j;
 	s = malloc((i + n + 1) * sizeof(char));
-	if (s != NULL )
+	if (s != NULL)
 	{
 		for (i = 0; s1[i] != '\0'; i++)
 			s[i] = s1[i];
