9ba2572a82a80c6518a3b6b0a0fae1f3e1e701a4
serosc95 <oscardleon95@gmail.com>
2020-07-09 17:54:17 +0000

    commit three

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,15 +9,16 @@
  */
 int *array_range(int min, int max)
 {
-	int *s, i;
+	int *s, i, size;
 
 	if (min > max)
 		return (NULL);
-	s = malloc((1 + max - min) * sizeof(int));
+	size = 1 + max - min;
+	s = malloc(size * sizeof(int));
 	if (s != NULL)
 	{
-		for (i = 0; i < (1 + max - min); i++)
-			s[i] = i;
+		for (i = 0; i < size; i++, min++)
+			s[i] = min;
 		return (s);
 	}
 	return (NULL);
