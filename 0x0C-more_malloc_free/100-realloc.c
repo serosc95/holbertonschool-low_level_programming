f0a73840f8caeb56c254c49145fc8ae1b2726d7e
serosc95 <oscardleon95@gmail.com>
2020-07-10 04:54:51 +0000

    commit advanced

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+#include"holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _realloc - reallocates a memory block using.
+ * @ptr: pointer.
+ * @old_size: integer.
+ * @new_size: integer.
+ * Return: void.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *s, *p = ptr;
+	unsigned int i;
+
+	if (ptr == NULL)
+	{
+		s = malloc(new_size);
+		return (s);
+	}
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+
+	s = malloc(new_size);
+	if (s != NULL)
+	{
+		for (i = 0; i < new_size; i++)
+			s[i] = p[i];
+		free(ptr);
+		return (s);
+	}
+	return (NULL);
+}
