414d191312bdbeafac3d84457d2d5e740483b1ae
serosc95 <oscardleon95@gmail.com>
2020-07-13 21:40:09 +0000

    commit four

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,13 +11,33 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *s;
+	int i = 0, j;
+	char *cp_name, *cp_owner;
 
 	s = malloc(sizeof(dog_t));
 	if (s != NULL)
 	{
-		s->name = name;
+		while (name[i] != '\0')
+			i++;
+		cp_name = malloc((i + 1) * sizeof(char));
+		i = 0;
+		while (owner[i] != '\0')
+			i++;
+		cp_owner = malloc((i + 1) * sizeof(char));
+		if (cp_name == NULL || cp_owner == NULL)
+		{
+			free(cp_name);
+			free(cp_owner);
+			free(s);
+			return (NULL);
+		}
+		for (j = 0; j <= i; j++)
+			cp_name[j] = name[j];
+		for (j = 0; j <= i; j++)
+			cp_owner[j] = owner[j];
+		s->name = cp_name;
 		s->age = age;
-		s->owner = owner;
+		s->owner = cp_owner;
 		return (s);
 	}
 	return (NULL);
