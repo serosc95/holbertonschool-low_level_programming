355c1572cb450d1cba6db631055add3a64894606
serosc95 <oscardleon95@gmail.com>
2020-07-13 18:53:06 +0000

    commit one

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include"dog.h"
+/**
+ * init_dog - prints a string.
+ * @d: pointer.
+ * @name: name dog.
+ * @age: age dog.
+ * @owner: owner dog.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
