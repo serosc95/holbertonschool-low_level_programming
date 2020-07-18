215c709d8d14251b245915625b7de0db415c908a
serosc95 <oscardleon95@gmail.com>
2020-07-18 18:26:02 +0000

    commit correction

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,15 +4,21 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * struct prin_t - Struct print
+ *
+ * @data: The operator
+ * @fun: The function associated
+ */
+typedef struct prin_t
+{
+	char *data;
+	void (*fun)(va_list);
+} prin_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-struct aawrep
-{
-	char *data;
-	void (*ptr)(va_list);
-};
 
 #endif
