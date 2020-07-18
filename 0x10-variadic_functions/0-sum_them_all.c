64ef87bad3a24cdebd2c04a2575dafb10a6d862b
serosc95 <oscardleon95@gmail.com>
2020-07-18 18:15:50 +0000

    correction betty

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,17 +8,17 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list valist;
-    int result = 0;
-    unsigned int i;
+	va_list valist;
+	int result = 0;
+	unsigned int i;
 
-    va_start(valist, n);
-    if (n == 0)
-    	return (0);
-    for (i = 0; i < n; i++)
-    {
-        result += va_arg(valist, int);
-    }
-    va_end(valist);
-    return (result);
+	va_start(valist, n);
+	if (n != 0)
+	{
+		for (i = 0; i < n; i++)
+			result += va_arg(valist, int);
+		va_end(valist);
+		return (result);
+	}
+	return (0);
 }
