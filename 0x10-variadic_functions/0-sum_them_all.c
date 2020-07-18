8a99afc5abfcda018b7e620a092c4b1f8e4b13f0
serosc95 <oscardleon95@gmail.com>
2020-07-18 18:12:23 +0000

    correction betty

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,7 +14,7 @@ int sum_them_all(const unsigned int n, ...)
 
     va_start(valist, n);
     if (n == 0)
-    return (0);
+    	return (0);
     for (i = 0; i < n; i++)
     {
         result += va_arg(valist, int);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,12 +4,6 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-typedef struct print_t
-{
-	char *data;
-	void (*fun)(va_list);
-} prin_t;
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, unsigned int n, ...);
