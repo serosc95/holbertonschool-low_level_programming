960d8f3e1769c0d72dbdda7521b9f8f3ec1ff528
serosc95 <oscardleon95@gmail.com>
2020-07-24 03:34:57 +0000

    advanced

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,9 @@
 #include"lists.h"
 /**
- * first - prnt forst that main.
+ * first - print first that main.
  */
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
