08333479062bcf7979007c9b06c569e73756a42b
serosc95 <oscardleon95@gmail.com>
2020-07-28 18:04:15 +0000

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,6 +14,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (!head)
 		return (NULL);
+	if (*head == NULL && idx != 0)
+		return (NULL);
 	newlist = malloc(sizeof(listint_t));
 	if (!newlist)
 		return (NULL);
@@ -25,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (newlist);
 	}
 	aux = *head;
-	for (i = 0; i <= idx - 1; i++)
+	for (i = 0; i < idx; i++)
 	{
 		if (aux)
 			aux = aux->next;
