89d9ac14cfba922ae73ed5fd58bc14313471aeeb
serosc95 <oscardleon95@gmail.com>
2020-07-28 17:29:27 +0000

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	aux = *head;
 	for (i = 0; i < idx; i++)
 	{
-		if (aux)
+		if (aux->next)
 			aux = aux->next;
 		else
 		{
