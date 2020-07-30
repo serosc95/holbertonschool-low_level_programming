08291e9717b8793f20a94cfb995c011021e1bc6a
serosc95 <oscardleon95@gmail.com>
2020-07-30 20:53:41 +0000

    prints the binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include"holberton.h"
+/**
+ * print_binary - prints the binary of a number.
+ * @n: is a number.
+ */
+void print_binary(unsigned long int n)
+{
+	int j;
+	unsigned long int aux;
+
+	if (n < 2)
+		_putchar(n + '0');
+	else
+	{
+		aux = n;
+		for (j = 0; aux > 0; j++)
+			aux = aux / 2;
+		for (j = j - 1; j >= 0; j--)
+		{
+			if ((n >> j) & 1)
+				_putchar('1');
+			else
+				_putchar('0');
+		}
+	}
+}
