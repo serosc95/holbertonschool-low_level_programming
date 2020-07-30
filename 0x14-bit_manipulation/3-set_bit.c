d0ea7ca70b64e8345ad0ce5efb061874775b869d
serosc95 <oscardleon95@gmail.com>
2020-07-30 23:55:06 +0000

    sets the value of a bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include"holberton.h"
+/**
+ * set_bit -  sets the value of a bit to 1.
+ * @n: is a number.
+ * @index: is a position.
+ * Return: number in the position.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int aux = 1;
+
+	if (index > 63)
+		return (-1);
+	for (;index > 0; index--)
+		aux *= 2;
+	*n += aux;
+	return (1);
+}
