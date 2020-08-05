8f1e895c7b0cb8ffb6a7007bd2217613122cdb79
serosc95 <oscardleon95@gmail.com>
2020-08-05 03:29:46 +0000

    copy file an another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,32 @@
 #include"holberton.h"
 /**
+ * close_file - close file.
+ * @file: is a file.
+ */
+void close_file(int file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
+	exit(100);
+}
+/**
+ * read_file - read file
+ * @file: is a file
+ */
+void read_file(char *file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+	exit(98);
+}
+/**
+ * write_file - write file
+ * @file: is a file
+ */
+void write_file(char *file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+	exit(99);
+}
+/**
  * main - check the code for Holberton School students.
  * @ac: number of arguments.
  * @av: arguments.
@@ -7,7 +34,7 @@
  */
 int main(int ac, char **av)
 {
-	int file_from, file_to, count = 0, aux;
+	int file_from, file_to, aux;
 	char str[1024];
 
 	if (ac != 3)
@@ -16,41 +43,23 @@ int main(int ac, char **av)
 		exit(97);
 	}
 	file_from = open(av[1], O_RDONLY);
-	if (file_from < 0 || count < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+	if (file_from < 0)
+		read_file(av[1]);
 	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+		write_file(av[2]);
 	aux = read(file_from, str, 1024);
 	while (aux > 0)
 	{
 		if (write(file_to, str, aux) < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			exit(99);
-		}
+			write_file(av[2]);
 		aux = read(file_from, str, 1024);
 	}
 	if (aux < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+		read_file(av[1]);
 	if (close(file_from) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
+		close_file(file_from);
 	if (close(file_to) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
+		close_file(file_to);
 	return (0);
 }
