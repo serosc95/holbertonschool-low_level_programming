f88dabed3a020eac8a9209415e645600659fa791
serosc95 <oscardleon95@gmail.com>
2020-08-05 02:23:02 +0000

    test

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,26 +1,24 @@
-#include "holberton.h"
+#include"holberton.h"
 /**
- * create_file - function that creates a file.
- * @filename: pointer to file.
+ * create_file - creates a file.
+ * @filename: filename.
  * @text_content: string.
- * Return: 1 or -1
+ * Return: number of characters.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int i;
+	int fd, count = 0;
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd < 0)
 		return (-1);
 	if (text_content)
 	{
-		for (i = 0; text_content[i] != '\0'; i++)
-		{
-		}
-		write(fd, text_content, i);
+		while (text_content[count])
+			count++;
+		write(fd, text_content, count);
 	}
 	close(fd);
 	return (1);
