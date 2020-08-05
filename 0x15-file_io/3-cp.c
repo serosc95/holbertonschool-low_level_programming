f8323ab89027854fdde30af1288cc214419918dd
serosc95 <oscardleon95@gmail.com>
2020-08-05 17:35:27 +0000

    pld

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,65 +1,57 @@
 #include"holberton.h"
 /**
- * close_file - close file.
- * @file: is a file.
- */
-void close_file(int file)
-{
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
-	exit(100);
-}
-/**
- * read_file - read file
- * @file: is a file
- */
-void read_file(char *file)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
-	exit(98);
-}
-/**
- * write_file - write file
- * @file: is a file
- */
-void write_file(char *file)
-{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
-	exit(99);
-}
-/**
  * main - check the code for Holberton School students.
- * @ac: number of arguments.
- * @av: arguments.
+ * @argc: number of arguments.
+ * @argv: arguments.
  * Return: Always 0.
  */
-int main(int ac, char **av)
+int main (int argc, char **argv)
 {
-	int file_from, file_to, aux;
-	char str[1024];
+	int file_from, file_to, wr;
+	char *buffer[1024];
+	int caracter = 1;
 
-	if (ac != 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	file_from = open(av[1], O_RDONLY);
-	if (file_from < 0)
-		read_file(av[1]);
-	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (file_to < 0)
-		write_file(av[2]);
-	aux = read(file_from, str, 1024);
-	while (aux > 0)
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
 	{
-		if (write(file_to, str, aux) < 0)
-			write_file(av[2]);
-		aux = read(file_from, str, 1024);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-	if (aux < 0)
-		read_file(av[1]);
-	if (close(file_from) < 0)
-		close_file(file_from);
-	if (close(file_to) < 0)
-		close_file(file_to);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while (caracter > 0)
+	{
+		caracter = read(file_from, buffer, 1024);
+		if (caracter == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		wr = write(file_to, buffer, caracter);
+		if (wr == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
 	return (0);
 }
