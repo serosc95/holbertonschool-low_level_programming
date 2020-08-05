6fbef052ccb3c5acc8e291d4c300c20a844dc051
serosc95 <oscardleon95@gmail.com>
2020-08-05 02:20:21 +0000

    test

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 	int i;
 
 	if (!filename)
-		return (-1)
+		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd < 0)
 		return (-1);
