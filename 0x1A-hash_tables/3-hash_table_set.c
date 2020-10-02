43c7ddc8fa9a5e1cdd2eb6be620cf5212d61d08c
serosc95 <oscardleon95@gmail.com>
2020-10-02 20:35:29 +0000

    Three tasks

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+/**
+ * add_node - adds a new node at the beginning of the list.
+ * @head: is the new node to add to the list.
+ * @key: hash number.
+ * @value: data value.
+ * Return: list of nodes.
+ */
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
+{
+	hash_node_t *newlist;
+
+	newlist = malloc(sizeof(hash_node_t));
+	if (newlist)
+	{
+		newlist->key = strdup(key);
+		newlist->value = strdup(value);
+		newlist->next = *head;
+		*head = newlist;
+		return (newlist);
+	}
+	return (NULL);
+}
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: is the table.
+ * @key: hash number.
+ * @value: data value.
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *newnode;
+	unsigned long int index;
+
+	if (!key || !ht || !value)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	newnode = add_node(&(ht->array[index]), key, value);
+	if (!newnode)
+		return (0);
+	ht->array[index] = newnode;
+	return (1);
+}
+
