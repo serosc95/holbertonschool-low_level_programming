88a34458b39dff7ce523151ca838712eab70aab8
serosc95 <oscardleon95@gmail.com>
2020-11-06 19:43:56 +0000

    corrections island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,12 +12,12 @@ def island_perimeter(grid):
                 down = i + 1
                 left = j - 1
                 rigth = j + 1
-                if grid[top][j] == 0:
+                if grid[top][j] == 0 or top < 0:
                     perimeter += 1
-                if grid[down][j] == 0:
+                if grid[down][j] == 0 or down == len(grid):
                     perimeter += 1
-                if grid[i][left] == 0:
+                if grid[i][left] == 0 or left < 0:
                     perimeter += 1
-                if grid[i][rigth] == 0:
+                if grid[i][rigth] == 0 or rigth == len(grid[i]):
                     perimeter += 1
     return perimeter
